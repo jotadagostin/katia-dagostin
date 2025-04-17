ab21ba409a9e3b79f94637dc45a0a41855f8dedb
João Paulo Dagostin <dagostinjota@gmail.com>
2025-04-17 18:20:16 +0200

    update header
    setting efect scrow with js script

---
+++ javascript/script.js
@@ -0,0 +1,9 @@
+// Function para efeito scroll do header:
+window.addEventListener("scroll", function () {
+  const header = document.querySelector("header");
+  if (window.scrollY > 50) {
+    header.classList.add("scrolled");
+  } else {
+    header.classList.remove("scrolled");
+  }
+});
