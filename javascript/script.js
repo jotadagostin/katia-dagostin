0de618e988f79bde3d7c993ae2b1b623d0789be8
João Paulo Dagostin <dagostinjota@gmail.com>
2025-04-21 09:21:04 +0200

    setting mobile version

--- javascript/script.js
+++ javascript/script.js
@@ -24,3 +24,21 @@ menuLinks.forEach((link) => {
 consultaBtn.addEventListener("click", () => {
   checkbox.checked = false;
 });
+
+// Smooth scrowlling:
+
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    const targetId = this.getAttribute("href");
+    const targetElement = document.querySelector(targetId);
+
+    if (targetElement) {
+      window.scrollTo({
+        top: targetElement.offsetTop,
+        behavior: "smooth",
+      });
+    }
+  });
+});
